082f764feb7f71eeb1e2241c3779bb76f30b67b0
Anna Horodetska and Eric Schoettle <eric@>
2017-04-20 16:09:09 -0700

    refactor code, use delete_if on array

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,18 +1,31 @@
 require "pry"
 
-class Fixnum
+# class Fixnum
+#   define_method(:primes) do
+#     array =*(2..self)
+#     to_remove = []
+#     array.each() do |prime|
+#       array.each do |number|
+#         if (number % prime == 0) && (prime != number)
+#           to_remove.push(number)
+#         end
+#       end
+#       prime += 1
+#     end
+#     array = array - to_remove
+#     array
+#   end
+# end
+
+
+class Float
   define_method(:primes) do
     array =*(2..self)
-    to_remove = []
-    array.each() do |prime|
-      array.each do |number|
-        if (number % prime == 0) && (prime != number)
-          to_remove.push(number)
-        end
-      end
+    prime = 2
+    until prime > Math.sqrt(self)
+      array.delete_if { |number| (number % prime == 0) && (prime != number) }
       prime += 1
     end
-    array = array - to_remove
     array
   end
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -3,6 +3,6 @@ require('./lib/primes')
 
 describe('Fixnum#primes') do
   it("gives primes below inputted number") do
-    expect(20.primes()).to(eq([2,3,5,7,11,13,17,19]))
+    expect(100.0.primes()).to(eq([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]))
   end
 end
