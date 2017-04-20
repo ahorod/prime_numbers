4b768a3d50db3ff34f5ea69ace72503240d1ef2a
Anna Horodetska and Eric Schoettle <anna+eric@>
2017-04-20 16:38:55 -0700

    add front end

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
-# require('sinatra')
-# require('sinatra/reloader')
-# also_reload('lib/**/*.rb')
-# require('./lib/coin')
-# require('pry')
-#
-# get('/') do
-#   erb(:index)
-# end
-#
-# get('/result') do
-#   @result = params.fetch('change').to_i.primes()
-#   erb(:result)
-# end
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/primes')
+require('pry')
+
+get('/') do
+  erb(:index)
+end
+
+get('/result') do
+  @result = params.fetch('input').to_f.primes()
+  erb(:result)
+end
